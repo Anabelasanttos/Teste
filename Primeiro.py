5759f9376a2398168de6f3c03e0350eb2e675755
Anabela Santos <Anabelasanttos@gmail.com>
2023-05-26 10:16:06 -0300

    Teste

---
+++ Primeiro.py
@@ -0,0 +1,13 @@
+# Comentario
+
+print("Hello")
+
+Linguagem = input('Qual a Linguagem de Programação que voce esta estudando?')
+
+
+"""
+Isso é um comentario
+
+"""
+
+print('A linguagem que voce esta estudando é', Linguagem)
---
+++ Variaveis.py
@@ -0,0 +1,10 @@
+# Variaveis
+
+Idade = 26 # Idade Recebe 26
+
+print(Idade)
+
+Nome = "Anabela Santos"
+
+print(Nome)
+
